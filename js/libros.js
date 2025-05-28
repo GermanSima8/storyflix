3a617e81e7b4934ddc4808c899ce4191e51cadb9
GermanSima8 <germansima8@gmail.com>
2025-05-27 22:20:13 -0600

    Nuevos cuentos

--- js/libros.js
+++ js/libros.js
@@ -6,7 +6,8 @@ const libros = {
       portada: "img/arbol.jpg",
       driveLink:
         "https://drive.google.com/file/d/19YKkd3YlRXMZCs4CidgvJWwJSKyiBBGp/view?usp=sharing  ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSck1SzSot8YNuvVUIf14VS2HkhkMOpXWeMo3hbp81nNh5E_oQ/viewform?usp=sharing&ouid=115465881991314726330",
       descripcion:
         "El misterio del árbol parlante cuenta la historia de Valentina, una niña exploradora que descubre que un árbol sabio necesita ayuda.",
     },
@@ -16,7 +17,8 @@ const libros = {
       portada: "img/capibara.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1M8bQX-qBgIGH5FHW9dAJ_bh_GxfA_eum/view?usp=sharing ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLScXL_btO92V2sRzR-Dbwt_K5HRhWJL4vCgrDgOAebuMAw3RxA/viewform?usp=sharing&ouid=115465881991314726330",
       descripcion:
         "Capibara y el secreto del río dorado narra la aventura de Luna, una capibara valiente que busca un río mágico.",
     },
@@ -26,7 +28,8 @@ const libros = {
       portada: "img/tortuga.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1G3irBHi5wVDu2MZ39etPLxSOD4sVJ-39/view?usp=sharing  ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdZrYC-AnsSWgqnLfO0qNOy-Y0z5YNHOlHIQNTmqzeLRnQTSw/viewform?usp=header",
       descripcion:
         "Carlos y la tortuga que sabía leer cuenta la historia de Carlos, un niño que no le gustaba leer, hasta que conoce a una tortuga mágica que le muestra que los libros son aventuras maravillosas",
     },
@@ -36,7 +39,8 @@ const libros = {
       portada: "img/bambi.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1_Mg7yEDMZJQ9j1__62Bt9Vu1zOfqpBgj/view?usp=sharing",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLScK-NICRGMOMf4e5f-b9cWs5p3HURQcD-ZHmUnV46y8MiODog/viewform?usp=sharing&ouid=115465881991314726330",
       descripcion:
         "Bambi es un tierno cuento de Disney sobre un cervatillo que crece en el bosque aprendiendo sobre la vida, la amistad y la pérdida.",
     },
@@ -56,7 +60,8 @@ const libros = {
       portada: "img/nemo.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1EjBBsbS-E6hxH4-ddiydGbGMEwG4cJYk/view?usp=sharing  ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSexOm0a9KyywlJ1QJ-OMBSu7bOkQ8azRiRAKXbgpH2pKjpPUQ/viewform?usp=header",
       descripcion:
         "Buscando a Nemo, un pez payaso que cruza el océano para encontrar a su hijo Nemo, capturado por un buzo. En el camino, hace nuevos amigos y aprende a confiar y dejar que su hijo crezca.",
     },
@@ -66,41 +71,108 @@ const libros = {
       portada: "img/dama.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1Z3aTIcc9GVmPFcxzGaOGXUPc1WvTkIYX/view?usp=sharing  ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdvF75Ex5k3m54qlr9Jf_C4NZB7LABn3X9BjKkpvRv-ockw9w/viewform?usp=dialog",
       descripcion:
         "La dama y el vagabundo es una encantadora historia de amor entre Reina, una perrita de hogar elegante, y Golfo, un perro callejero libre y aventurero.",
+    },
+    {
+      id: 8,
+      titulo: "Un charco azul",
+      portada: "img/charco.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1o9sYma94xCGh3qhlgz1-YL_aCUoiogY_/view",
+      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      descripcion:
+        "Cuenta la historia de un charco que se forma en el suelo después de una tormenta, y cómo diferentes personajes, incluyendo animales y personas, interactúan con él.",
     },
   ],
   masLeidos: [
     {
-      id: 8,
+      id: 9,
       titulo: "Goofy e hijo",
       portada: "img/goofy.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1p2aathPiBiNiviBPsncclvdQrxMM-zRG/view?usp=sharing",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSfBco5aN2P49Q0NBjZFASgPsfuPtI2r3yda6FO-8YRiYylu0A/viewform?usp=header",
       descripcion:
         "Goofy e Hijo es un divertido cuento, donde Goofy intenta acercarse a su hijo Max llevándolo a un viaje por carretera.",
     },
     {
-      id: 9,
+      id: 10,
       titulo: "Tierra de osos",
       portada: "img/osos.jpeg",
       driveLink:
         "https://drive.google.com/file/d/1DpUZ-pbL5TgCz0IaSHuabGij5Lo9X8Fg/view?usp=sharing",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdiW0mMoLMCGDVSKAqEraQQhufGer1JeTJhQLj5fDNFO2wQTQ/viewform?usp=header",
       descripcion:
         "Tierra de osos es una emotiva historia de Disney sobre Kenai, un joven que, tras un acto impulsivo, es transformado en oso.",
     },
     {
-      id: 9,
+      id: 11,
       titulo: "El rey león",
       portada: "img/leon.jpg",
       driveLink:
         "https://drive.google.com/file/d/1rNgeiRAqQULYzL0WjvPhv876BfO4cSir/view?usp=sharing  ",
-      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSex8soXYRZ9HpSRzHb1EP4VlW6MrXP7OOPlpin7sfQuMLOzJw/viewform?usp=header",
       descripcion:
         "El Rey León es la historia de Simba, un leoncito travieso que un día será rey.",
+    },
+    {
+      id: 12,
+      titulo: "A qué sabe la luna",
+      portada: "img/luna.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/12vizpYSS4ylS69gGSVVC8eg1BkLND1_9/view?usp=drivesdk",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdNcdLMLdYcD4XnOF9jpiOHHir5V-l3kFF3ES9qkixFBZUuKA/viewform?usp=header",
+      descripcion:
+        "A qué sabe la luna es un cuento infantil que trata sobre la curiosidad de un grupo de animales por saber a qué sabe la luna.",
+    },
+    {
+      id: 13,
+      titulo: "NO (es así)",
+      portada: "img/esasi.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1ChEtn1kVUyciU4SJb5nESTZdK8J8f0y4/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdNcdLMLdYcD4XnOF9jpiOHHir5V-l3kFF3ES9qkixFBZUuKA/viewform?usp=header",
+      descripcion:
+        "Narra la historia de un niño que se cuestiona la realidad y busca demostrar que las cosas no son como se le han dicho.",
+    },
+    {
+      id: 14,
+      titulo: "El príncipe ceniciento",
+      portada: "img/ceniciento.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1NKH2su0-YK-6GkG1NrbHjUtFUgLLpeam/view",
+      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      descripcion:
+        "El príncipe es un chico delgado que debe hacer las tareas del hogar mientras sus hermanos grandes se divierten. Un hada le concede un deseo.",
+    },
+    {
+      id: 15,
+      titulo: "El lobo que quería cambiar de color",
+      portada: "img/lobo.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1S8ElUUQpGgWxr1JpEOnBnpJno8MVy8zq/view",
+      formLink: "https://docs.google.com/forms/d/ID_FORMULARIO_1",
+      descripcion:
+        "Narra la historia de un lobo que, insatisfecho con su color original negro, decide cambiar de color.",
+    },
+    {
+      id: 16,
+      titulo: "¿Puede Pedro puercoespín controlar su mal genio?",
+      portada: "img/pedro.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1kAJW099aLW3D5dqU6J4JBFzCrVVica5c/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSe4L3ccHuM6Oqaa4WLxN2UYXYtgnPExy_NAMxkSEY3alm-CbA/viewform",
+      descripcion:
+        "Pedro el Puercoespín tenía un MAL GENIO. Los amigos de Pedro tienen que agacharse cuando su cara se pone roja y comienza a temblar.",
     },
   ],
 };
