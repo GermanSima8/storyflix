847c2482f276f13cde7e50fd68931ab2e5c02507
GermanSima8 <germansima8@gmail.com>
2025-06-03 22:49:23 -0600

    Cuentos listos

--- js/libros.js
+++ js/libros.js
@@ -88,10 +88,76 @@ const libros = {
       descripcion:
         "Cuenta la historia de un charco que se forma en el suelo después de una tormenta, y cómo diferentes personajes, incluyendo animales y personas, interactúan con él.",
     },
+    {
+      id: 9,
+      titulo: "La solidaridad",
+      portada: "img/solidaridad.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1ub_SnwVBtPugniNjH1ux7PFPtxwMdcjD/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSf-eqPbCk2A_qLbAwQOVFJtJXvK7BzsPzybhcgpagkr4Rg5jg/viewform?usp=sharing&ouid=115465881991314726330",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 10,
+      titulo: "Cuento un hombre pobre",
+      portada: "img/pobre.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/18jZBHJbooIJN6H6qQaNrp9gDp_6pHm2Q/view?usp=sharing",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLScdlrKwZqseoZgLDEiN0hLiAR5-J7E38Ry5XwkJOgk6F6_4mQ/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 11,
+      titulo: "La amistad es compartir",
+      portada: "img/amistad.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/17-b5zMfnWqBMCfWbcSGm8pA1FtrqxAOB/view?usp=sharing",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdSxTkNiI-8ZbwE57dgnK4rFTaHmnD3-RYyP25ULToVmD0WvQ/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 12,
+      titulo: "La cabra peluda",
+      portada: "img/cabra.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1y3eLxZIaSC2RTrOtKSm6l__kw1hgIc92/view?usp=sharing",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSe7u7OGQDq4sHctjEMnMY657R4FeTzcp58Y07rJLfQOh-6Qsw/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 13,
+      titulo: "El desfile del alebrije",
+      portada: "img/alebrije.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/170a3A6jBOcsoD3fURc6NWQj7YYJ4lh9c/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSf00n55AQKoud5t6RSMoww1DfE07RaEXEXk7EAG-dPvONXo8g/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 14,
+      titulo: "El gato chelo",
+      portada: "img/chelo.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1EN1LO_dr9grc2UKzko24vOWv1y7lqzyo/view?usp=sharing",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSfNUyWghO_apGlGNNVtrgoM2tVq2RkTIO3KoML_qMnS076bOg/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
   ],
   masLeidos: [
     {
-      id: 9,
+      id: 15,
       titulo: "Goofy e hijo",
       portada: "img/goofy.jpeg",
       driveLink:
@@ -102,7 +168,7 @@ const libros = {
         "Goofy e Hijo es un divertido cuento, donde Goofy intenta acercarse a su hijo Max llevándolo a un viaje por carretera.",
     },
     {
-      id: 10,
+      id: 16,
       titulo: "Tierra de osos",
       portada: "img/osos.jpeg",
       driveLink:
@@ -113,7 +179,7 @@ const libros = {
         "Tierra de osos es una emotiva historia de Disney sobre Kenai, un joven que, tras un acto impulsivo, es transformado en oso.",
     },
     {
-      id: 11,
+      id: 17,
       titulo: "El rey león",
       portada: "img/leon.jpg",
       driveLink:
@@ -124,7 +190,7 @@ const libros = {
         "El Rey León es la historia de Simba, un leoncito travieso que un día será rey.",
     },
     {
-      id: 12,
+      id: 18,
       titulo: "A qué sabe la luna",
       portada: "img/luna.jpg",
       driveLink:
@@ -135,7 +201,7 @@ const libros = {
         "A qué sabe la luna es un cuento infantil que trata sobre la curiosidad de un grupo de animales por saber a qué sabe la luna.",
     },
     {
-      id: 13,
+      id: 19,
       titulo: "NO (es así)",
       portada: "img/esasi.jpg",
       driveLink:
@@ -146,7 +212,62 @@ const libros = {
         "Narra la historia de un niño que se cuestiona la realidad y busca demostrar que las cosas no son como se le han dicho.",
     },
     {
-      id: 14,
+      id: 20,
+      titulo: "El rescatista",
+      portada: "img/rescatista.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1DTYfJQitarOfyIdWECby-QY1IYoazZf9/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSeowT4_iDm3af6qQKYjMwt-_IWGzeUAG1Ry1laAqJR3Xrn2lw/viewform?usp=sharing&ouid=115465881991314726330",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 21,
+      titulo: "La gama ciega",
+      portada: "img/gama.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1rBpc3lyZCKKCCm5b6M2dEZgnrTr5WyKv/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdVs3Y-ZeYSbNvhZDjZxoYRGIgWQAD9NiKBg0jP7ck28JuuDQ/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 22,
+      titulo: "El rosal",
+      portada: "img/rosal.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/17zWkRhl8INVDTScuLv6oe1H69fU9QKWv/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSeb0TpjvBIfFq39fSprNdu8Q5JRc5c9i_SIr9eJYaXb543uqg/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 23,
+      titulo: "La campanilla de invierno",
+      portada: "img/campanilla.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1b86LWFFcHV3GK48H70MhPRwhEod9TPWs/view?usp=sharing",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSfGi-XhsgSdQmww4jD4eG5QrtW1idAfYwsNVn-BeU_y1fcBJg/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. ",
+    },
+    {
+      id: 25,
+      titulo: "Hermano y hermana",
+      portada: "img/brothers.jpg",
+      driveLink:
+        "https://drive.google.com/file/d/1xtmgPvESVyWNuPxFLCOklf7e8DNDJ1mY/view",
+      formLink:
+        "https://docs.google.com/forms/d/e/1FAIpQLSf695sbJ7asT4xm4u68XVz1aAdxq7CAS11WrTDJnVNk996uzA/viewform?usp=header",
+      descripcion:
+        "Estas historias, por lo general, se centran en experiencias, personajes, y situaciones que, aunque breves, generan reflexiones sobre la vida, la sociedad, y la condición humana. .",
+    },
+    {
+      id: 26,
       titulo: "El príncipe ceniciento",
       portada: "img/ceniciento.jpg",
       driveLink:
@@ -157,7 +278,7 @@ const libros = {
         "El príncipe es un chico delgado que debe hacer las tareas del hogar mientras sus hermanos grandes se divierten. Un hada le concede un deseo.",
     },
     {
-      id: 15,
+      id: 27,
       titulo: "El lobo que quería cambiar de color",
       portada: "img/lobo.jpg",
       driveLink:
@@ -168,7 +289,7 @@ const libros = {
         "Narra la historia de un lobo que, insatisfecho con su color original negro, decide cambiar de color.",
     },
     {
-      id: 16,
+      id: 28,
       titulo: "¿Puede Pedro puercoespín controlar su mal genio?",
       portada: "img/pedro.jpg",
       driveLink:
